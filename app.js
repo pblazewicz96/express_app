9f5cd77d0e2c0d229bdcbf3db390b976ae330e6a
Piotr <pblazewicz96@gmail.com>
2021-03-17 01:11:41 +0100

    admin login, news

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var createError = require("http-errors");
+var cookieSession = require("cookie-session");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var config = require("./config");
+var mongoose = require("mongoose");
+
+mongoose.connect(config.db, {
+  useNewUrlParser: true,
+  // useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   console.log("db connect");
+// });
 
 var indexRouter = require("./routes/index");
 var newsRouter = require("./routes/news");
@@ -20,6 +34,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: config.keySession,
+    maxAge: config.maxAgeSession, // 24 hours
+  })
+);
 
 //funkcja ktora jest routem, pobiera z param req aktualny adres strony i przekazuje go do kazdego widoku
 app.use(function (req, res, next) {
